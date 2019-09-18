b5745115f1800df93a03fea45f51e8f150c7babe
Sanoj Silva <silvasanoj@gmail.com>
2019-09-18 15:47:40 +0530

    Added new sections

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	setHeroAnimations();
+	setCountdown();
+});
+
+function setHeroAnimations() {
 	anime({
 		targets: "#day-title .first-4 path",
 		strokeDashoffset: [anime.setDashoffset, 0],
@@ -50,7 +55,7 @@ $(document).ready(function() {
 		strokeDashoffset: [anime.setDashoffset, 0],
 		easing: "easeInOutSine",
 		duration: 1000,
-		fill: ["#fff", "#0098a6"],
+		fill: ["#fff", "rgb(148, 39, 167)"],
 		delay: 2000,
 		update: function(anim) {
 			console.log(anim.progress);
@@ -58,4 +63,63 @@ $(document).ready(function() {
 		direction: "alternate",
 		loop: false
 	});
-});
+	anime({
+		targets: "#man-2 path",
+		strokeDashoffset: [anime.setDashoffset, 0],
+		easing: "easeInOutSine",
+		duration: 1000,
+		fill: ["#fff", "rgb(148, 39, 167)"],
+		delay: 2000,
+		update: function(anim) {
+			console.log(anim.progress);
+		},
+		direction: "alternate",
+		loop: false
+	});
+	anime({
+		targets: "#man-3 path",
+		strokeDashoffset: [anime.setDashoffset, 0],
+		easing: "easeInOutSine",
+		duration: 1000,
+		fill: ["#fff", "rgb(148, 39, 167)"],
+		delay: 2000,
+		update: function(anim) {
+			console.log(anim.progress);
+		},
+		complete: function() {
+			anime({
+				targets: "#man-3",
+				rotate: {
+					value: 180,
+					duration: 5000
+				}
+				// translateX: -250
+			});
+		},
+		direction: "alternate",
+		loop: false
+	});
+}
+
+function setCountdown() {
+	var ieeeDay = new Date("Oct 1, 2019 00:00:00").getTime();
+	var daysEl = document.getElementById("days");
+	var hoursEl = document.getElementById("hours");
+	var minsEl = document.getElementById("mins");
+	var secsEl = document.getElementById("secs");
+
+	setInterval(function() {
+		var now = new Date().getTime();
+		var distance = ieeeDay - now;
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor(
+			(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var secs = Math.floor((distance % (1000 * 60)) / 1000);
+		daysEl.innerText = days;
+		hoursEl.innerText = hours;
+		minsEl.innerText = mins;
+		secsEl.innerText = secs;
+	}, 1000);
+}
